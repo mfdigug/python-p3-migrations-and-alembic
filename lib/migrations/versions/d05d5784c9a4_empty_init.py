a9947f42dde0919bf3a02ea14e7b7e71c7b54abb
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-01 12:47:46 +1100

    done with migrations and alembic lab

---
+++ lib/migrations/versions/d05d5784c9a4_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: d05d5784c9a4
+Revises: 
+Create Date: 2026-02-01 12:27:34.856034
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd05d5784c9a4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
